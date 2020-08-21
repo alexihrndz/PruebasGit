89f9fb4d643ea52a37d2b9feb72fb9458854fa59
Alexi Ruben Hernandez Ochoa <arhernandez.qvision@gmail.com>
2020-08-21 11:53:07 -0500

    otro commit Nuevos cmabios en rama 2

--- src/main/java/com/co/qv/steps/nuevoStep.java
+++ src/main/java/com/co/qv/steps/nuevoStep.java
@@ -1,8 +1,11 @@
 package com.co.qv.steps;
 
+import net.thucydides.core.annotations.Step;
+
 public class nuevoStep {
     //nuevosCambios2
 
+    @Step
     public void otherMethod(){
         System.out.println(getClass().toString());
     }
