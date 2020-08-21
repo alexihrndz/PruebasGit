bc55a3341dcb291ed50fdc50d6a2444fd2f71873
Alexi Ruben Hernandez Ochoa <arhernandez.qvision@gmail.com>
2020-08-21 18:52:22 -0500

    Nuevo commit

--- src/main/java/com/co/qv/steps/NuevoStep.java
+++ src/main/java/com/co/qv/steps/NuevoStep.java
@@ -1,34 +1,34 @@
 package com.co.qv.steps;
 
 import net.thucydides.core.annotations.Step;
-
-import java.time.Instant;
+import org.joda.time.Instant;
 
 public class NuevoStep {
 
     @Step
     public void nuevoMetodo() {
         Instant hora = Instant.now();
-        System.out.println(hora);
+        System.out.println(hora.toString().substring(1, 20));
     }
+
     @Step
-    public void otherMethod(){
+    public void otherMethod() {
 
     }
 
     @Step
-    public void otherMethod80(){
+    public void otherMethod80() {
         String a = "nueva cadena";
         String b = a.replace("nueva", "new");
         System.out.println(b);
     }
 
     @Step
-    public void otraPrueba(){
+    public void otraPrueba() {
 
     }
 
-    public void unMetodo(){
+    public void unMetodo() {
 
     }
 }
