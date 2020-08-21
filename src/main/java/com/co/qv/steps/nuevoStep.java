26b4b1f6e8aa7696887b8778ce06d960f7a000e7
Alexi Ruben Hernandez Ochoa <arhernandez.qvision@gmail.com>
2020-08-21 16:14:02 -0500

    Cambios

--- src/main/java/com/co/qv/steps/nuevoStep.java
+++ src/main/java/com/co/qv/steps/nuevoStep.java
@@ -6,21 +6,19 @@ public class nuevoStep {
 
     @Step
     public void nuevoMetodo() {
-        System.out.println("Pruebas pull y merge");
-        System.out.println("Nueva linea");
 
     }
     @Step
     public void otherMethod(){
-        System.out.println(getClass().toString());
+
     }
 
     @Step
     public void otraPrueba(){
-        System.out.println("Cambio en otro método");
+
     }
 
     public void unMetodo(){
-        System.out.println("Cambio en otro método");
+
     }
 }
