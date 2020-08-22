eb3cd6d74aa78c99e2576f2b94807680fe91fb04
Alexi Ruben Hernandez Ochoa <arhernandez.qvision@gmail.com>
2020-08-21 19:25:53 -0500

    Nuevo commit

--- src/main/java/com/co/qv/steps/NuevoStep.java
+++ src/main/java/com/co/qv/steps/NuevoStep.java
@@ -18,7 +18,7 @@ public class NuevoStep {
 
     @Step
     public void otherMethod80() {
-        String a = "nueva cadena";
+        String a = "nueva cadena1";
         String b = a.replace("nueva", "new");
         System.out.println(b);
     }
