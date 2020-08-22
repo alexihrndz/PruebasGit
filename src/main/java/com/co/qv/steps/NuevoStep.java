8b0178e92c435e27d18a006b993fe074f0683afd
Alexi <59544641+arhernandezqv@users.noreply.github.com>
2020-08-22 12:13:50 -0500

    Update NuevoStep.java

--- src/main/java/com/co/qv/steps/NuevoStep.java
+++ src/main/java/com/co/qv/steps/NuevoStep.java
@@ -20,7 +20,7 @@ public class NuevoStep {
     public void otherMethod80() {
         String a = "nueva cadena1";
         String b = a.replace("nueva", "new");
-        System.out.println(b);
+        System.out.println(b+"Otra cosa");
     }
 
     @Step
